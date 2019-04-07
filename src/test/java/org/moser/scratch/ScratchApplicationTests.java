0fef93de6c6e2e04c3779e5b83a5c8e63dc0f941
chrismoser <chrisaaronmoser@gmail.com>
2019-04-07 10:56:06 -0500

    added 100% code coverage

--- src/test/java/org/moser/scratch/ScratchApplicationTests.java
+++ src/test/java/org/moser/scratch/ScratchApplicationTests.java
@@ -6,11 +6,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ScratchApplication.class)
 public class ScratchApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		ScratchApplication.main(new String[]{
+				"--spring.main.web-environment=false"
+		});
 	}
-
 }
